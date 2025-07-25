4fe2a8dcea0749f77bc8d37eafb7ac051f4afb0f
José Noguera <juniornoguera1@gmail.com>
2025-07-24 21:17:20 -0600

    Finalizacion de opcion 1

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -2,16 +2,25 @@
 #en progra orientada a objetos se le dice metodos
 def promedio():
     nums = []
+    negative_nums = 0
+    positive_nums = 0
     while True:
         action = input("1. Ingresar número\n2. Ya ingresé todos los numeros\n")
         match action:
             case "1":
                 num = int(input("Ingresa el número: "))
                 nums.append(num)
+                if num < 0:
+                    negative_nums += 1
+                elif num > 0:
+                    positive_nums += 1
             case "2":
                 break
+            case _:
+                print("Número no válido")
     print(f"1. La suma de todos los numeros es de {sum(nums)}")
     print(f"2. El promedio es de {sum(nums)/len(nums)}")
-    print(f"3. La cantidad de numeros positivos es de ")
+    print(f"3. La cantidad de numeros positivos es de {positive_nums} y la cantidad de números negativos es de {negative_nums} ")
+
 
 
